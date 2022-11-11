dacace7bbdf7ea37eee1766e050cfa7d134420fd
marcoBmota8 <marcobarberomota@gmail.com>
2022-11-10 20:41:34 -0600

    modifications to the supplemental ground truth and plotted it

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 PROTEIN_MAPPING = {
     'raf': 0,
     'mek': 1,
@@ -11,6 +13,18 @@ PROTEIN_MAPPING = {
     'p38': 9,
     'jnk': 10
 }
+
+def generate_edges(graph, mapping):
+    edges = []
+
+    # for each node in graph
+    for node in graph:
+
+        # for each neighbour node of a single node
+        for neighbour in graph[node]:
+            # if edge exists then append
+            edges.append((mapping[node],mapping[neighbour]))
+    return edges
 
 def get_sachs_model_adj_matrix():
     matrix = [[0 for _ in range(11)] for _ in range(11)]
@@ -36,25 +50,29 @@ def get_sachs_model_adj_matrix():
 
             matrix[index_1][index_2] = 1
 
-    return matrix 
+    return np.array(matrix) 
 
 def get_supplemented_ground_truth_adj_matrix():
-    matrix = [[0 for _ in range(11)] for _ in range(11)]
+    matrix = np.zeros((11,11))
 
     connections = {
-        'raf': ['mek', 'akt'],
-        'mek': ['erk', 'akt', 'plc', 'jnk'],
-        'plc': ['pip2', 'pip3', 'akt', 'mek', 'pka'],
-        'pip2': [],
-        'pip3': ['pip2'],
+        'raf': ['mek','akt'],
+        'mek': ['erk','jnk','plc'],
+        'plc': ['pip2','mek','pkc', 'pka', 'akt'],
+        'pip2': ['pkc'],
+        'pip3': ['pip2', 'akt','plc'],
         'erk': ['akt'],
-        'akt': ['raf',' mek', 'plc'],
-        'pka': ['raf', 'akt', 'erk', 'jnk', 'mek', 'p38', 'plc'],
-        'pkc': ['raf', 'jnk', 'mek', 'p38', 'akt'],
+        'akt': ['mek','pkc','raf','plc'],
+        'pka': ['raf', 'akt', 'erk', 'jnk', 'mek', 'p38','plc','pkc'],
+        'pkc': ['raf', 'jnk', 'mek', 'p38','akt','pka'],
         'p38': ['jnk'],
-        'jnk': ['mek', 'p38']
+        'jnk': ['p38','mek']
     }
     
+    #get the nodes labels
+    nodes = connections.keys()
+
+
     # building ground truth matrix
     for protein_1, protein_list in connections.items():
         for protein_2 in protein_list:
@@ -62,4 +80,4 @@ def get_supplemented_ground_truth_adj_matrix():
 
             matrix[index_1][index_2] = 1
 
-    return matrix
+    return matrix, generate_edges(connections, PROTEIN_MAPPING),  nodes
